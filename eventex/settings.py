413ed2896726618944b6ced687b180982b0c8f27
Beatriz <beatriz@victors-imac.globoprdom.com.br>
2016-09-01 17:37:39 -0300

    allowed host

--- eventex/settings.py
+++ eventex/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DUBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
