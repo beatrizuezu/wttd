189c0b5bfd10430d1e621e5487d895d1a2460f95
Beatriz <beatriz@victors-imac.globoprdom.com.br>
2016-09-19 11:14:38 -0300

    tela de inscricao

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from eventex.core import views as eventex_views
+from eventex.subscriptions.views import subscribe
+
 
 urlpatterns = [
 	url(r'^$', eventex_views.home),
     url(r'^admin/', admin.site.urls),
+    url(r'^inscricao/$',subscribe)
 ]
